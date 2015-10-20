1399d118d44bb2f2620edc1f541822532e5f795a
ChenZhang <ckcz12345@gmail.com>
2015-10-20 22:59:31 +0800

    取消分享到人人

--- app/src/main/java/com/pkuhelper/lib/Share.java
+++ app/src/main/java/com/pkuhelper/lib/Share.java
@@ -1,46 +1,22 @@
 package com.pkuhelper.lib;
 
-//import java.io.*;
-
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
-//import android.graphics.Bitmap.CompressFormat;
 import android.graphics.drawable.BitmapDrawable;
 import android.util.Log;
 
 import com.pkuhelper.R;
-//import com.pkuhelper.lib.view.CustomToast;
-/*
-import com.renn.rennsdk.RennClient;
-import com.renn.rennsdk.RennExecutor.CallBack;
-import com.renn.rennsdk.RennResponse;
-import com.renn.rennsdk.RennClient.LoginListener;
-import com.renn.rennsdk.exception.RennException;
-import com.renn.rennsdk.param.*;
-*/
 import com.tencent.mm.sdk.openapi.*;
 import com.tencent.mm.sdk.modelmsg.*;
 
 public class Share {
-	// We do not provide any share to renren now
-	private static final String[] sharesTo={"发送给微信好友","分享到朋友圈"/*,"分享到人人"*/};
+	private static final String[] sharesTo={"发送给微信好友","分享到朋友圈"};
 	private static final String WX_APP_ID="wxe410b2da07aaa0e4";
-	/*
-	private static final String RENREN_APP_ID="475741";
-	private static final String RENREN_API_KEY="83469c9420c4408088221971b64fefde";
-	private static final String RENREN_SECRET_KEY="be71e223026945d2b2db18bf15d2f5ac";
-	private static final String RENREN_PERMISSIONS="publish_feed photo_upload status_update read_user_photo publish_share";
-	
-	private static final int RENREN_SHARE_URL=0;
-	private static final int RENREN_SHARE_IMAGE=1;
-	private static final int RENREN_SHARE_TEXT=2;
-	private static final String addString="(分享自PKU Helper)";
-	*/
+
 
 	private static IWXAPI api;
-	//private static RennClient rennClient;
 	
 	public static void readyToShareURL(final Context context, final String dialogTitle,
 			final String url, final String title, final String content, final Bitmap bitmap) {
@@ -53,8 +29,6 @@ public class Share {
 					urlToWx(context, url, title, content, bitmap, false);
 				else if (which==1)
 					urlToWx(context, url, title, content, bitmap, true);
-//				else if (which==2)
-//					shareToRenren(context, RENREN_SHARE_URL, title, url, null);
 			}
 		}).setCancelable(true).show();
 	}
@@ -70,8 +44,6 @@ public class Share {
 					imgToWx(context, bitmap, false);
 				else if (which==1)
 					imgToWx(context, bitmap, true);
-//				else if (which==2)
-//					shareToRenren(context, RENREN_SHARE_IMAGE, dialogTitle, "renren_photo", bitmap);
 			}
 		}).setCancelable(true).show();
 	}
@@ -86,8 +58,6 @@ public class Share {
 					textToWx(context, text, false);
 				else if (which==1)
 					textToWx(context, text, true);
-//				else if (which==2)
-//					shareToRenren(activity, RENREN_SHARE_TEXT, text, "", null);
 			}
 		}).setCancelable(true).show();
 	}
@@ -154,137 +124,5 @@ public class Share {
 		
 		return api;
 	}
-/*
-	public static void shareToRenren(final Context context,
-			final int type, final String text, final String url, final Bitmap bitmap) {
-		if (rennClient==null) {
-			rennClient=RennClient.getInstance(context);
-			rennClient.init(RENREN_APP_ID, RENREN_API_KEY, RENREN_SECRET_KEY);
-			rennClient.setScope(RENREN_PERMISSIONS);
-			//rennClient.setTokenType("bearer");
-			rennClient.setLoginListener(new LoginListener() {
-				@Override
-				public void onLoginSuccess() {
-					realShareToRenren(context, type, text, url, bitmap);
-				}
-				@Override
-				public void onLoginCanceled() {				
-					rennClient=null;
-				}
-			});
-			rennClient.login(context);
-		}
-		else realShareToRenren(context, type, text, url, bitmap);
-	}
-	private static void realShareToRenren(final Context context,
-			int type, String text, String url, Bitmap bitmap) {
-		if (rennClient==null) return;
-		if (!rennClient.isLogin())  {
-			rennClient=null; shareToRenren(context, type, text, url, bitmap);
-			return;
-		}
-		if (type==RENREN_SHARE_URL) {
-			String string=text+" "+addString;
-			PutShareUrlParam putShareUrlParam=new PutShareUrlParam();
-			putShareUrlParam.setUrl(url);
-			putShareUrlParam.setComment(string);
-			try {
-			rennClient.getRennService().sendAsynRequest(putShareUrlParam, new CallBack() {
-				
-				@Override
-				public void onSuccess(RennResponse arg0) {
-					//Toast.makeText(activity, "分享成功！", Toast.LENGTH_SHORT).show();
-					CustomToast.showInfoToast(context, "分享成功！");
-				}
-				
-				@Override
-				public void onFailed(String arg0, String arg1) {
-					String text="分享失败";
-					if (arg1!=null && !"".equals(arg1)) text=arg1;
-					CustomToast.showErrorToast(context, text);
-
-				}
-			});
-			} catch (RennException e) {
-				e.printStackTrace();
-				CustomToast.showErrorToast(context, "分享失败");
-			}
-		}
-		else if (type==RENREN_SHARE_IMAGE) {
-			if (bitmap==null) return;
-			File file=MyFile.getFile(context, "cache", Util.getHash(url));
-			try {
-				file.createNewFile();
-				FileOutputStream fileOutputStream=new FileOutputStream(file);
-				bitmap.compress(CompressFormat.JPEG, 100, fileOutputStream);
-				fileOutputStream.flush();
-				fileOutputStream.close();
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-				CustomToast.showErrorToast(context, "分享失败");
-				return;
-			}
-			if (text==null || "".equals(text)) text=addString;
-			else text+=" "+addString;
-			
-			UploadPhotoParam uploadPhotoParam=new UploadPhotoParam();
-			uploadPhotoParam.setFile(file);
-			uploadPhotoParam.setDescription(text);
-			try {
-				rennClient.getRennService().sendAsynRequest(uploadPhotoParam, new CallBack() {
-					
-					@Override
-					public void onSuccess(RennResponse arg0) {
-						CustomToast.showSuccessToast(context, "分享成功！");
-					}
-					
-					@Override
-					public void onFailed(String arg0, String arg1) {
-						//if (arg1!=null && !"".equals(arg1)) 
-						//	Toast.makeText(activity, arg1, Toast.LENGTH_SHORT).show();
-						//else Toast.makeText(activity, "分享失败", Toast.LENGTH_SHORT).show();
-						String text="分享失败";
-						if (arg1!=null && !"".equals(arg1)) text=arg1;
-						CustomToast.showErrorToast(context, text);
-					}
-				});
-			} catch (RennException e) {
-				e.printStackTrace();
-				//Toast.makeText(activity, "分享失败", Toast.LENGTH_SHORT).show();
-				CustomToast.showErrorToast(context, "分享失败");
-			}
-		}
-		else if (type==RENREN_SHARE_TEXT) {
-			String string=text+" "+addString;
-			PutStatusParam putStatusParam=new PutStatusParam();
-			putStatusParam.setContent(string);
-			try {
-				rennClient.getRennService().sendAsynRequest(putStatusParam, new CallBack() {
-					
-					@Override
-					public void onSuccess(RennResponse arg0) {
-						//Toast.makeText(activity, "发布成功！", Toast.LENGTH_SHORT).show();
-						CustomToast.showSuccessToast(context, "发布成功！");
-					}
-					
-					@Override
-					public void onFailed(String arg0, String arg1) {
-						//if (arg1!=null && !"".equals(arg1)) 
-						//	Toast.makeText(activity, arg1, Toast.LENGTH_SHORT).show();
-						//else Toast.makeText(activity, "发布失败", Toast.LENGTH_SHORT).show();
-						String text="发布失败";
-						if (arg1!=null && !"".equals(arg1)) text=arg1;
-						CustomToast.showErrorToast(context, text);
-					}
-				});
-			} catch (RennException e) {
-				e.printStackTrace();
-				//Toast.makeText(activity, "发布失败", Toast.LENGTH_SHORT).show();
-				CustomToast.showErrorToast(context, "发布失败");
-			}
-		}
-	}
-*/
 }
 
