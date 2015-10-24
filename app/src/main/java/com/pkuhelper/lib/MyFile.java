edba10a9425e2feeace7bb87a80046d8f7ea9c32
ChenZhang <ckcz12345@gmail.com>
2015-10-24 13:50:07 +0800

    修复MyFile的null闪退

--- app/src/main/java/com/pkuhelper/lib/MyFile.java
+++ app/src/main/java/com/pkuhelper/lib/MyFile.java
@@ -49,7 +49,8 @@ public class MyFile {
 	}
 
 	private static File getFilesDir(Context context) {
-		return useSDCard ? context.getExternalFilesDir(null) : context.getFilesDir();
+		return useSDCard && Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())?
+				context.getExternalFilesDir(null) : context.getFilesDir();
 	}
 
 	public static File getFile(Context context, String _dir, String _name) {
@@ -95,7 +96,8 @@ public class MyFile {
 	}
 
 	public static File getCache(Context context, String name) {
-		File cacheDir = useSDCard ? context.getExternalCacheDir() : context.getCacheDir();
+		File cacheDir = useSDCard && Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())
+				? context.getExternalCacheDir() : context.getCacheDir();
 		return name == null || "".equals(name) ? cacheDir : new File(cacheDir, name);
 	}
 
