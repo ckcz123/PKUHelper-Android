9dbb767c746237a501e48ae92f1b2afdba5e602b
Ziyang Xu <xuziyang@pku.edu.cn>
2015-12-04 14:14:37 +0800

    add Material-hole item

---
+++ app/src/main/java/com/pkuhelper/M_PKUhole/MHoleActivity.java
@@ -0,0 +1,16 @@
+package com.pkuhelper.M_pkuhole;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.pkuhelper.R;
+
+public class MHoleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mhole);
+    }
+
+}
