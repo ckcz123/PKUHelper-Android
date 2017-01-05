4cfd025bdc8e4cbb5dca76630ec1f0f02f387a77
ckcz123 <ckcz123@126.com>
2017-01-05 22:59:43 +0800

    2.1 更换域名

--- app/src/main/java/com/pkuhelper/lib/Constants.java
+++ app/src/main/java/com/pkuhelper/lib/Constants.java
@@ -9,10 +9,10 @@ import com.pkuhelper.Settings;
 import java.util.ArrayList;
 
 public class Constants {
-	public static final String version = "2.0.6.1";
+	public static final String version = "2.1";
 	public static final String update_time = "2017-01-05";
 
-	public static final String domain = "http://www.xiongdianpku.com";
+	public static final String domain = "http://www.pkuhelper.com";
 	public static final String bbsurl = "https://bbs.pku.edu.cn/client/bbsclient.php";
 
 	public static final int REQUEST_REPORT = 300;
